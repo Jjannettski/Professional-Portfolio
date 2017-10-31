0efeeae7a7dc4975f6a45c7115a784f10df07444
james <jjannettski@gmail.com>
2017-10-31 12:32:58 -0400

    a bunch of shit i updated

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,6 @@
 $(document)
     .ready(function() {
-
 $('.ui.styled.fluid.accordion').accordion();
-
 $(".ui.large.top.fixed.hidden.menu").hide();
       // fix menu when passed
       $('.masthead')
@@ -16,37 +14,30 @@ $(".ui.large.top.fixed.hidden.menu").hide();
           }
         })
       ;
-
       // create sidebar and attach to menu open
       $('.ui.sidebar')
         .sidebar('attach events', '.toc.item')
       ;
-
       $("#hire-me-form").hide();
-
 function hireMeFormGenerator(){
-
   $("#hire-me-form").show();
   $("#story-form").hide();
-
 }
 function storyFormGenerator(){
-
   $("#story-form").show();
   $("#hire-me-form").hide();
 
 }
-
       $("#hire-me-button").on("click",function(){
-
         hireMeFormGenerator();
-
       });
       $("#story-button").on("click",function(){
-
         storyFormGenerator();
-
       });
+
+$("#story-button").on("click", function(){
+  
+});
 
     })
   ;
